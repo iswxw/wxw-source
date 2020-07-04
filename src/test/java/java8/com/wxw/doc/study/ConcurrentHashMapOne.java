c0643e9e906ddc83de091ae972b4782d29de8105
魏小伟 <1550518923@qq.com>
2020-07-04 22:40:37 +0800

    java8

---
+++ src/test/java/java8/com/wxw/doc/study/ConcurrentHashMapOne.java
@@ -0,0 +1,15 @@
+package com.wxw.doc.study;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @Author: wxw
+ * @create: 2020-04-24-11:03
+ */
+public class ConcurrentHashMapOne {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+        concurrentHashMap.put("1", "A");
+
+    }
+}
