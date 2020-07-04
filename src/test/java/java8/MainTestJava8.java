923d8c46efee8feb0dfb7cf9020ef49d93b750f5
魏小伟 <1550518923@qq.com>
2020-07-04 19:43:22 +0800

    jdk 8/11/13

---
+++ src/test/java/java8/MainTestJava8.java
@@ -0,0 +1,24 @@
+package java8;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ Author ：wxw.
+ * @ Date ： 19:35 2020/7/4
+ * @ Description：Java8 测试
+ * @ Version:   v_0.0.1
+ */
+public class MainTestJava8{
+    public static void main(String[] args) {
+
+        /**
+         * 记录
+         * （1）HashMap
+         *
+         */
+        Map<String, String> map = new HashMap<String, String>();
+
+
+    }
+}
