6ff84a4448c8e08d880539751973a5b17e663357
魏小伟 <1550518923@qq.com>
2020-08-22 17:17:06 +0800

    线程池的使用

---
+++ src/main/java/java8/com/wxw/code/concurrent/threadpoll/ThreadPoolMain.java
@@ -0,0 +1,17 @@
+package java8.com.wxw.code.concurrent.threadpoll;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @ Author ：wxw.
+ * @ Date ： 16:27 2020/8/22
+ * @ Description：线程池的使用
+ * @ Version:   v_0.0.1
+ */
+public class ThreadPoolMain {
+    public static void main(String[] args) {
+
+        ThreadPoolExecutor threadPoolExecutor;
+
+    }
+}
