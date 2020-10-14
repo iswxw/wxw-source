9e6c5b67cb1883b0f6d134c1085afc57bcb3e89b
魏小伟 <1550518923@qq.com>
2020-10-14 17:36:57 +0800

    Math类的方法基本使用

--- src/main/java/java8/com/wxw/code/general/MathDemo.java
+++ src/main/java/java8/com/wxw/code/general/MathDemo.java
@@ -13,11 +13,15 @@ public class MathDemo {
          * Java Math.round() 四舍五入
          * Java Math.random() 获取随机数
          * java Math.pow()计算次方
-         * Java Math.ceil() 取整
+         * Java Math.ceil() 取整 向上取证
          * Java Math.sqrt() 计算平方根
          * Java Math.log() 计算对数
          * Java Math.floor() 取整
          */
         System.out.println("Math.abs(10) 获取绝对值 = " + Math.abs(-10));
+        System.out.println("Math.ceil(5/2) 向上取整 = " + Math.ceil(5 / 2));
+        System.out.println("Math.round(5/2) = " + Math.round(5 / 2));
+        System.out.println("Math.floor(5/2) = " + Math.floor(5 / 2));
+        System.out.println("(Integer)(5/2) = " + (Integer)(5 / 2));
     }
 }
