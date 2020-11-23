3948db21c7e24cea4c36e0d663a3309a101ac74d
魏小伟 <1550518923@qq.com>
2020-11-23 22:40:24 +0800

    reentrantlock

--- src/main/java/java8/com/wxw/code/concurrent/lock/ReentrantLock01.java
+++ src/main/java/java8/com/wxw/code/concurrent/lock/ReentrantLock01.java
@@ -9,10 +9,28 @@ import java.util.concurrent.locks.ReentrantLock;
  * @ Version:   v_0.0.1
  */
 public class ReentrantLock01 {
-    public static void main(String[] args) {
 
-        // ReentrantLock  // https://blog.csdn.net/qq_41893274/article/details/105554149
-        ReentrantLock reentrantLock = new ReentrantLock();
-        reentrantLock.lock();
+    // ReentrantLock
+    // https://blog.csdn.net/qq_41893274/article/details/105554149
+    public static ReentrantLock reentrantLock = new ReentrantLock();
+    int count = 0;
+
+    public static void main(String[] args) {
+        ReentrantLock01 lock = new ReentrantLock01();
+        lock.run();
     }
+
+    public void run() {
+        // 加锁
+        reentrantLock.lock();
+        try {
+            for (int i = 0; i < 1000; i++) {
+                count++;
+            }
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+
+
 }
