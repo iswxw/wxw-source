ab7f7abf2733e5bb95dd2d670998f82274e656cd
魏小伟 <1550518923@qq.com>
2020-11-25 09:50:17 +0800

    ReentrantLock 20201125

--- src/main/java/java8/com/wxw/code/concurrent/lock/reentrant/ReentrantLock_Sync.java
+++ src/main/java/java8/com/wxw/code/concurrent/lock/reentrant/ReentrantLock_Sync.java
@@ -4,22 +4,24 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author wxw
+ * @author Dell
  * @ Date ： 13:49 2020/9/18
  * @ Description：jdk级别的锁  csdn：https://blog.csdn.net/qq_41893274/article/details/105554149
  * @ Version:   v_0.0.1
- * @author Dell
  */
-public class ReentrantLock01 {
+public class ReentrantLock_Sync {
     // ReentrantLock
     public static ReentrantLock reentrantLock = new ReentrantLock(true);
     public static int count = 0;
 
     public static void main(String[] args) {
-        ReentrantLock01 lock = new ReentrantLock01();
-        lock.run();
+
+        // 同步功能
+        run();
         System.out.println("计算结果 = " + count);
     }
-    public void run() {
+
+    public static void run() {
         // 加锁
         reentrantLock.lock();
         try {
