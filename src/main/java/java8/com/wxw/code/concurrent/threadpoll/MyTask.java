98ace2e9d88229c7084388915f4b1bd6bae6acf6
魏小伟 <1550518923@qq.com>
2020-11-29 16:08:51 +0800

    线程池 demo

---
+++ src/main/java/java8/com/wxw/code/concurrent/threadpoll/MyTask.java
@@ -0,0 +1,23 @@
+package java8.com.wxw.code.concurrent.threadpoll;
+
+/**
+ * @author: wxw
+ * @date: 2020-11-29-16:05
+ */
+public class MyTask implements Runnable {
+    private int num;
+    public MyTask(int num) {
+        this.num = num;
+    }
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        System.out.println("正在执行任务"+num);
+        try {
+            Thread.currentThread().sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("任务"+num+"执行完毕");
+    }
+}
