22129771b31cefe185ab74ae85c43ad33ee230ef
魏小伟 <1550518923@qq.com>
2020-11-29 15:59:21 +0800

    线程池

--- src/main/java/java8/com/wxw/code/study/ThreadPoolOne.java
+++ src/main/java/java8/com/wxw/code/study/ThreadPoolOne.java
@@ -1,11 +1,17 @@
 package java8.com.wxw.code.study;
 
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * @Author: wxw
  * @create: 2020-04-25-23:53
  */
 public class ThreadPoolOne {
     public static void main(String[] args) {
+//        ThreadPoolExecutor
+//        Executors
         System.out.println("查看CPU核数:"+Runtime.getRuntime().availableProcessors());
     }
 }
