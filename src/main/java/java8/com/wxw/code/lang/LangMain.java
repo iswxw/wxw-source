447ba5ef15d09e18d0bcf7d1310d57972b33bedc
魏小伟 <1550518923@qq.com>
2020-12-06 12:12:34 +0800

    ConcurrentHashMap v1

---
+++ src/main/java/java8/com/wxw/code/lang/LangMain.java
@@ -0,0 +1,11 @@
+package java8.com.wxw.code.lang;
+
+/**
+ * @author: wxw
+ * @date: 2020-12-06-11:54
+ */
+public class LangMain {
+    public static void main(String[] args) {
+        ThreadLocal threadLocal = new ThreadLocal();
+    }
+}
---
+++ src/main/java/java8/com/wxw/code/lang/threadlocal/ThreadLocalTest.java
@@ -0,0 +1,11 @@
+package java8.com.wxw.code.lang.threadlocal;
+
+/**
+ * @author: wxw
+ * @date: 2020-12-06-11:54
+ */
+public class ThreadLocalTest {
+    public static void main(String[] args) {
+
+    }
+}
